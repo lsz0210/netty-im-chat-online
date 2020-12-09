1fa8bb1a9acc62a4c47e65e681408c1b2f9e1a17
lishizhao <lishizhao@haier.com>
2020-12-09 20:49:42 +0800

    首次提交，实现无心跳无连接时长限制的在线聊天功能

---
+++ src/main/java/com/duck/im/chat/netty/ChatHandler.java
@@ -0,0 +1,114 @@
+package com.duck.im.chat.netty;
+
+import com.duck.im.chat.core.enums.MsgActionEnum;
+import com.duck.im.chat.utils.JsonUtils;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * 
+ * @Description: 处理消息的handler
+ * TextWebSocketFrame： 在netty中，是用于为websocket专门处理文本的对象，frame是消息的载体
+ */
+public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+	// 用于记录和管理所有客户端的channle
+	public static ChannelGroup users = 
+			new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) 
+			throws Exception {
+		// 获取客户端传输过来的消息
+		String content = msg.text();
+		System.out.println("客户端传入的消息" + content);
+
+		Channel currentChannel = ctx.channel();
+
+		// 1. 获取客户端发来的消息
+		DataContent dataContent = JsonUtils.jsonToPojo(content, DataContent.class);
+		Integer action = dataContent.getAction();
+		// 2. 判断消息类型，根据不同的类型来处理不同的业务
+
+		if (action.equals(MsgActionEnum.CONNECT.type)) {
+			// 	2.1  当websocket 第一次open的时候，初始化channel，把用的channel和userid关联起来
+			String senderId = dataContent.getChatMsg().getSenderId();
+			System.out.println("客户端连接，用户id为 " + senderId);
+
+			UserChannelRel.put(senderId, currentChannel);
+			
+			// 测试
+			for (Channel c : users) {
+				System.out.println(c.id().asLongText());
+			}
+			UserChannelRel.output();
+		} else if (action.equals(MsgActionEnum.CHAT.type)) {
+			//  2.2  聊天类型的消息，把聊天记录保存到数据库，同时标记消息的签收状态[未签收]
+			ChatMsg chatMsg = dataContent.getChatMsg();
+			String msgText = chatMsg.getMsg();
+			String receiverId = chatMsg.getReceiverId();
+			String senderId = chatMsg.getSenderId();
+			
+			// 后续需要保存消息到数据库
+
+			DataContent dataContentMsg = new DataContent();
+			dataContentMsg.setChatMsg(chatMsg);
+			dataContentMsg.setAction(action);
+			
+			// 发送消息
+			// 从全局用户Channel关系中获取接受方的channel
+			Channel receiverChannel = UserChannelRel.get(receiverId);
+			if (receiverChannel == null) {
+				// TODO channel为空代表用户离线，推送消息（JPush，个推，小米推送）
+			} else {
+				// 当receiverChannel不为空的时候，从ChannelGroup去查找对应的channel是否存在
+				Channel findChannel = users.find(receiverChannel.id());
+				if (findChannel != null) {
+					// 用户在线
+					receiverChannel.writeAndFlush(
+							new TextWebSocketFrame(
+									JsonUtils.objectToJson(dataContentMsg)));
+				} else {
+					// 用户离线 TODO 推送消息
+				}
+			}
+			
+		}
+		else if (action.equals(MsgActionEnum.KEEPALIVE.type)) {
+			//  2.4  心跳类型的消息
+			System.out.println("收到来自channel为[" + currentChannel + "]的心跳包...");
+		}
+	}
+	
+	/**
+	 * 当客户端连接服务端之后（打开连接）
+	 * 获取客户端的channle，并且放到ChannelGroup中去进行管理
+	 */
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		users.add(ctx.channel());
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		
+		String channelId = ctx.channel().id().asShortText();
+		System.out.println("客户端被移除，channelId为：" + channelId);
+		
+		// 当触发handlerRemoved，ChannelGroup会自动移除对应客户端的channel
+		users.remove(ctx.channel());
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		// 发生异常之后关闭连接（关闭channel），随后从ChannelGroup中移除
+		ctx.channel().close();
+		users.remove(ctx.channel());
+	}
+}
